070cdedb1980eb46801f95c8ca13d282e79313f1
Pan Liu <us.panliu@gmail.com>
2022-10-20 00:24:13 -0700

    add single select html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Select from './Select';
+
+const options = [
+  {label: "XS", value: "XS"},
+  {label: "S", value: "S"},
+  {label: "M", value: "M"},
+  {label: "L", value: "L"},
+  {label: "XL", value: "XL"},
+]
 
 function App() {
+  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+
   return (
-    <h1>Select</h1>
+    <div>
+      <h1>Select</h1>
+      <Select options={options} value={value} onChange={o => setValue(o)} />
+    </div>
   )
 }
 
-export default App
+export default App;
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,5 +1,34 @@
-function Select() {
+import styles from "./select.module.css";
+import { TiTimes } from 'react-icons/ti';
+import { FaCaretDown } from 'react-icons/fa';
 
+type SelectOption = {
+  label: string;
+  value: string;
+}
+
+type SelectProps = {
+  options: SelectOption[];
+  onChange: (value: SelectOption | undefined) => void;
+  value?: SelectOption;
+}
+
+function Select({ options, onChange, value }: SelectProps) {
+  return (
+    <div tabIndex={0} className={styles.container}>
+      <span className={styles.value}>{value?.label}</span>
+      <TiTimes className={styles.delete}/>
+      <div className={styles.divider}></div>
+      <FaCaretDown className={styles.caret}/>
+      <ul className={`${styles.options} ${styles.show}`}>
+        {options.map(o => (
+          <li key={o.label} className={styles.option}>
+            {o.value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Select;
