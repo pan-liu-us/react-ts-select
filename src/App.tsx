1ad3d9bef29c999c436486390f4c1d02b6576c43
Pan Liu <us.panliu@gmail.com>
2022-10-21 00:05:12 -0700

    add multiple select

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import { useState } from 'react';
-import './App.css';
-import Select from './Select';
+import { useState } from "react";
+import "./App.css";
+import Select, { SelectOption } from "./Select";
 
 const options = [
-  {label: "SELECT A SIZE", value: "SELECT A SIZE"},
-  {label: "XS", value: "XS"},
-  {label: "S", value: "S"},
-  {label: "M", value: "M"},
-  {label: "L", value: "L"},
-  {label: "XL", value: "XL"},
-]
+  { label: "SELECT A SIZE", value: "SELECT A SIZE" },
+  { label: "XS", value: "XS" },
+  { label: "S", value: "S" },
+  { label: "M", value: "M" },
+  { label: "L", value: "L" },
+  { label: "XL", value: "XL" },
+];
+
+const skills = [
+  { label: "JavaScript", value: "JavaScript" },
+  { label: "Python", value: "Python" },
+  { label: "HTML", value: "HTML" },
+  { label: "CSS", value: "CSS" },
+  { label: "React", value: "React" },
+  { label: "Node", value: "Node" },
+  { label: "RESTful API", value: "RESTful API" },
+  { label: "AWS", value: "AWS" },
+  { label: "MongoDB", value: "MongoDB" },
+  { label: "MySQL", value: "MySQL" },
+  { label: "PostgreSQL", value: "PostgreSQL" },
+];
 
 function App() {
-  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+  const [singleValue, setSingleValue] = useState<SelectOption | undefined>(
+    options[0]
+  );
+  const [multiValue, setMultiValue] = useState<SelectOption[]>([]);
 
   return (
     <div>
       <h1>Size</h1>
-      <Select options={options} value={value} onChange={o => setValue(o)} />
+      <Select
+        options={options}
+        value={singleValue}
+        onChange={(o) => setSingleValue(o)}
+      />
+      <div style={{ height: "12em" }} />
+      <h1>Skills</h1>
+      <Select
+        multiple
+        options={skills}
+        value={multiValue}
+        onChange={(o) => setMultiValue(o)}
+      />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,52 +1,98 @@
 import { useState } from "react";
 import styles from "./select.module.css";
-import { TiTimes } from 'react-icons/ti';
-import { FaCaretDown } from 'react-icons/fa';
+import { TiTimes } from "react-icons/ti";
+import { FaCaretDown } from "react-icons/fa";
 
-type SelectOption = {
+export type SelectOption = {
   label: string;
   value: string;
-}
+};
+
+type MultipleSelectProps = {
+  multiple: true;
+  value: SelectOption[];
+  onChange: (value: SelectOption[]) => void;
+};
+
+type SingleSelectProps = {
+  multiple?: false;
+  value?: SelectOption;
+  onChange: (value: SelectOption | undefined) => void;
+};
 
 type SelectProps = {
   options: SelectOption[];
-  onChange: (value: SelectOption | undefined) => void;
-  value?: SelectOption;
-}
+} & (SingleSelectProps | MultipleSelectProps);
 
-function Select({ options, onChange, value }: SelectProps) {
+function Select({ multiple, options, onChange, value }: SelectProps) {
   const [isOpen, setIsOpen] = useState(false);
 
+  function selectOption(option: SelectOption) {
+    if (multiple) {
+      if (value.includes(option)) {
+        onChange(value.filter((o) => o !== option));
+      } else {
+        onChange([...value, option]);
+      }
+    } else {
+      if (option !== value) onChange(option);
+    }
+  }
+
   return (
-    <div
-      tabIndex={0}
-      className={styles.container}>
-        <span className={styles.value}>{value?.label}</span>
-        <TiTimes
-          className={styles.delete}
-          onClick={e => onChange(options[0])}/>
-        <div
-          className={styles.divider}>
-        </div>
-        <FaCaretDown
-          className={styles.caret}
-          onClick={() => setIsOpen(prev => !prev)}/>
-        <ul
-          className={`${styles.options} ${isOpen ? styles.show : ""}`}>
-            {options.slice(1).map(o => (
+    <div tabIndex={0} className={styles.container}>
+      <span className={styles.value}>
+        {multiple
+          ? value.map((o) => (
+              <button
+                key={o.label}
+                className={styles["option-badge"]}
+                onClick={(e) => selectOption(o)}
+              >
+                {o.value}
+                <TiTimes />
+              </button>
+            ))
+          : value?.value}
+      </span>
+      <TiTimes
+        className={styles.delete}
+        onClick={(e) => (multiple ? onChange([]) : onChange(options[0]))}
+      />
+      <div className={styles.divider}></div>
+      <FaCaretDown
+        className={styles.caret}
+        onClick={() => setIsOpen((prev) => !prev)}
+      />
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+        {multiple
+          ? options.map((o) => (
               <li
                 key={o.label}
                 className={styles.option}
-                onClick={e => {
-                  o !== value ? onChange(o) : undefined;
+                onClick={(e) => {
+                  selectOption(o);
                   setIsOpen(false);
-                }}>
-                  {o.value}
+                }}
+              >
+                {o.value}
+              </li>
+            ))
+          : options.slice(1).map((o) => (
+              <li
+                key={o.label}
+                className={styles.option}
+                onClick={(e) => {
+                  selectOption(o);
+                  setIsOpen(false);
+                }}
+              >
+                {o.value}
               </li>
             ))}
-        </ul>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Select;
